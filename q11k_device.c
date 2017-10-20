fb62707169d57d615efcae3fb67da8187c7d35fd
Konata Izumi <konachan.700@gmail.com>
2017-10-20 22:16:41 +0300

    add correct init procedure (it increase resolution)

--- q11k_device.c
+++ q11k_device.c
@@ -21,68 +21,14 @@
 #define MODULENAME                     "q11k_device"
 #define DEVNAME                        "Huion Q11K Tablet"
 
-#define USB_VENDOR_ID_HUION		       0x256c
-#define USB_DEVICE_ID_HUION_TABLET	   0x006e
+#define USB_VENDOR_ID_HUION		        0x256c
+#define USB_DEVICE_ID_HUION_TABLET	    0x006e
+
+#define CONFIG_BUF_SIZE                 514
 
 #define DEBUG
-
-#define Q11K_RDESC_ORIG_SIZE1 83
-static __u8 q11k_rdesc_fixed1[] = {
-    0x05, 0x0D,        // Usage Page (Digitizer)
-    0x09, 0x02,        // Usage (Pen)
-    0xA1, 0x01,        // Collection (Application)
-    0x85, 0x0A,        //   Report ID (10)
-    0x09, 0x20,        //   Usage (Stylus)
-    0xA1, 0x00,        //   Collection (Physical)
-    0x09, 0x42,        //     Usage (Tip Switch)
-    0x09, 0x44,        //     Usage (Barrel Switch)
-    0x09, 0x45,        //     Usage (Eraser)
-    0x09, 0x3C,        //     Usage (Invert)
-    0x09, 0x43,        //     Usage (Secondary Tip Switch)
-    0x09, 0x44,        //     Usage (Barrel Switch)
-    0x15, 0x00,        //     Logical Minimum (0)
-    0x25, 0x01,        //     Logical Maximum (1)
-    0x75, 0x01,        //     Report Size (1)
-    0x95, 0x06,        //     Report Count (6)
-    0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x09, 0x32,        //     Usage (In Range)
-    0x75, 0x01,        //     Report Size (1)
-    0x95, 0x01,        //     Report Count (1)
-    0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x81, 0x03,        //     Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x05, 0x01,        //     Usage Page (Generic Desktop Ctrls)
-    0x09, 0x30,        //     Usage (X)
-    0x09, 0x31,        //     Usage (Y)
-    0x55, 0x0D,        //     Unit Exponent (-3)
-    0x65, 0x33,        //     Unit (System: English Linear, Length: Inch)
-    0x26, 0xFF, 0x7F,  //     Logical Maximum (32767)
-    0x35, 0x00,        //     Physical Minimum (0)
-    0x46, 0x00, 0x08,  //     Physical Maximum (2048)
-    0x75, 0x10,        //     Report Size (16)
-    0x95, 0x02,        //     Report Count (2)
-    0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x05, 0x0D,        //     Usage Page (Digitizer)
-    0x09, 0x30,        //     Usage (Tip Pressure)
-    0x26, 0xFF, 0x1F,  //     Logical Maximum (8191)
-    0x75, 0x10,        //     Report Size (16)
-    0x95, 0x01,        //     Report Count (1)
-    0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,              //   End Collection
-    0xC0,              // End Collection
-};
-
-#define Q11K_RDESC_ORIG_SIZE0 18
-static __u8 q11k_rdesc_fixed0[] = {
-    0x06, 0x00, 0xFF,  // Usage Page (Vendor Defined 0xFF00)
-    0x09, 0x01,        // Usage (0x01)
-    0xA1, 0x01,        // Collection (Application)
-    0x85, 0x08,        //   Report ID (8)
-    0x75, 0x58,        //   Report Size (88)
-    0x95, 0x01,        //   Report Count (1)
-    0x09, 0x01,        //   Usage (0x01)
-    0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,              // End Collection
-};
+#define DPRINT(d, ...)       printk(d, ##__VA_ARGS__)
+#define DPRINT_DEEP(d, ...)  //printk(d, ##__VA_ARGS__)
 
 #define Q11K_KEYMAP_SIZE 11
 static unsigned short def_keymap[Q11K_KEYMAP_SIZE] = {
@@ -102,14 +48,16 @@ int
 static int q11k_probe(struct hid_device *hdev, const struct hid_device_id *id) {
     int rc, i;
 	struct usb_interface *intf = to_usb_interface(hdev->dev.parent);
+    struct usb_device *usb_dev = interface_to_usbdev(intf);
     unsigned long quirks = id->driver_data;
     int if_number = intf->cur_altsetting->desc.bInterfaceNumber;
+    char buf[CONFIG_BUF_SIZE];
     
     hdev->quirks |= HID_QUIRK_MULTI_INPUT;
 	hdev->quirks |= HID_QUIRK_NO_EMPTY_INPUT;
     
     if (id->product == USB_DEVICE_ID_HUION_TABLET) {
-        printk("q11k device detected if=%d", if_number);
+        DPRINT("q11k device detected if=%d", if_number);
         
         hid_set_drvdata(hdev, (void *)quirks);
         
@@ -119,7 +67,7 @@ static int q11k_probe(struct hid_device *hdev, const struct hid_device_id *id) {
             return rc;
         }
         
-        rc = hid_hw_start(hdev, HID_CONNECT_HIDRAW);//HID_CONNECT_DEFAULT);
+        rc = hid_hw_start(hdev, HID_CONNECT_HIDRAW);
         if (rc) {
             hid_err(hdev, "hw start failed\n");
             return rc;
@@ -140,7 +88,7 @@ static int q11k_probe(struct hid_device *hdev, const struct hid_device_id *id) {
             
             input_set_drvdata(idev, hdev);
             
-            idev->name = "Huion Q11K Tablet";
+            idev->name       = "Huion Q11K Tablet";
             idev->id.bustype = BUS_USB;
             idev->id.vendor  = 0x56a;
             idev->id.version = 0;
@@ -153,11 +101,10 @@ static int q11k_probe(struct hid_device *hdev, const struct hid_device_id *id) {
             set_bit(BTN_STYLUS, idev->keybit);
             set_bit(BTN_STYLUS2, idev->keybit);
             
-            input_set_abs_params(idev, ABS_X, 1, 32640, 0, 0);
-            input_set_abs_params(idev, ABS_Y, 1, 32640, 0, 0);
+            input_set_abs_params(idev, ABS_X, 1, 55662, 0, 0);
+            input_set_abs_params(idev, ABS_Y, 1, 34789, 0, 0);
             input_set_abs_params(idev, ABS_PRESSURE, 1, 8192, 0, 0);
 
-            
             rc = input_register_device(idev);
             if (rc) {
                 hid_err(hdev, "error registering the input device\n");
@@ -165,23 +112,33 @@ static int q11k_probe(struct hid_device *hdev, const struct hid_device_id *id) {
                 return rc;
             }
         } else if (if_number == 0) {
+            rc = usb_string(usb_dev, 0x02, buf, CONFIG_BUF_SIZE);
+            if (rc > 0) DPRINT("String(0x02) = %s", buf);
+            
+            rc = usb_string(usb_dev, 0xc9, buf, 256);
+            if (rc > 0) DPRINT("String(0xc9) = %s", buf);
+            
+            rc = usb_string(usb_dev, 0xc8, buf, 256);
+            if (rc > 0) DPRINT("String(0xc8) = %s", buf);
+            
+            rc = usb_string(usb_dev, 0xca, buf, 256);
+            if (rc > 0) DPRINT("String(0xca) = %s", buf);
+            
             idev_keyboard = input_allocate_device();
             if (idev_keyboard == NULL) {
                 hid_err(hdev, "failed to allocate input device [kb]\n");
                 return -ENOMEM;
             }
             
-            idev_keyboard->name = "Huion Q11K Keyboard";
-            idev_keyboard->id.bustype = BUS_USB;
-            idev_keyboard->id.vendor  = 0x04b4;
-            idev_keyboard->id.version = 0;
-            idev_keyboard->keycode = def_keymap;
-            idev_keyboard->keycodemax  = Q11K_KEYMAP_SIZE;
-            idev_keyboard->keycodesize = sizeof(def_keymap[0]);
+            idev_keyboard->name                 = "Huion Q11K Keyboard";
+            idev_keyboard->id.bustype           = BUS_USB;
+            idev_keyboard->id.vendor            = 0x04b4;
+            idev_keyboard->id.version           = 0;
+            idev_keyboard->keycode              = def_keymap;
+            idev_keyboard->keycodemax           = Q11K_KEYMAP_SIZE;
+            idev_keyboard->keycodesize          = sizeof(def_keymap[0]);
             
             input_set_capability(idev_keyboard, EV_MSC, MSC_SCAN);
-            //input_set_capability(idev_keyboard, EV_REL, MSC_SCAN);
-            //input_set_capability(idev_keyboard, EV_REP, MSC_SCAN);
             
             for (i=0; i<Q11K_KEYMAP_SIZE; i++) {
                 input_set_capability(idev_keyboard, EV_KEY, def_keymap[i]);
@@ -195,38 +152,13 @@ static int q11k_probe(struct hid_device *hdev, const struct hid_device_id *id) {
             }
         } 
         
-        printk("q11k device ok");
+        DPRINT("q11k device ok");
     } else {
-        printk("q11k hid strange error");
+        DPRINT("q11k hid strange error");
         return -ENODEV;
     }
     
     return 0;
-}
-
-static __u8 *q11k_report_fixup(struct hid_device *hdev, __u8 *rdesc, unsigned int *rsize) {
-    struct usb_interface *iface = to_usb_interface(hdev->dev.parent);
-    if (hdev->product == USB_DEVICE_ID_HUION_TABLET) {
-        switch (iface->cur_altsetting->desc.bInterfaceNumber) {
-            case 0:
-                printk("USB_DEVICE_ID_HUION_TABLET (rsize=%d, size=%d)", *rsize, Q11K_RDESC_ORIG_SIZE0);
-                if (*rsize == Q11K_RDESC_ORIG_SIZE0) {
-                    rdesc = q11k_rdesc_fixed0;
-                    *rsize = sizeof(q11k_rdesc_fixed0);
-                    printk("USB_DEVICE_ID_HUION_TABLET 0 OK");
-                }
-                break;
-            case 1:
-                printk("USB_DEVICE_ID_HUION_TABLET (rsize=%d, size=%d)", *rsize, Q11K_RDESC_ORIG_SIZE1);
-                if (*rsize == Q11K_RDESC_ORIG_SIZE1) {
-                    rdesc = q11k_rdesc_fixed1;
-                    *rsize = sizeof(q11k_rdesc_fixed1);
-                    printk("USB_DEVICE_ID_HUION_TABLET 1 OK");
-                }
-                break;            
-        }
-    }
-    return rdesc;
 }
 
 static void __q11k_rkey_press(unsigned short key, int b_key_raw, int s) {
@@ -241,92 +173,91 @@ static void q11k_rkey_press(unsigned short key, int b_key_raw) {
     __q11k_rkey_press(key, b_key_raw, 0);
 }
 
+static void __upress_pen(void) {
+    if (stylus_pressed == 1) {
+        input_report_key(idev_keyboard, BTN_MIDDLE, 0);
+        input_report_key(idev_keyboard, BTN_RIGHT, 0);
+        input_sync(idev_keyboard);
+    }
+    stylus_pressed = 0;
+}
+
 static int q11k_raw_event(struct hid_device *hdev, struct hid_report *report, u8 *data, int size) {
-    int key_raw = 0, pressure, x_pos, y_pos;
+    int pressure, x_pos, y_pos;
     
     if ((idev_keyboard == NULL) || (idev == NULL)) return -ENODEV;
     
-    //printk("q11k_raw_event %d -- %d -- Buf: %*phC", size, key_raw, size, data);
-    //printk("hid_report %d -- %d -- Buf: %*phC", report->maxfield, (int) sizeof(struct hid_report), (int) sizeof(struct hid_report), report);
+    DPRINT_DEEP("q11k_raw_event: %d\t%*phC", size, size, data);
     
-    if ((size == 8) && (data[0] == 0x0a) && (
-        (data[1] == 0xc1) || (data[1] == 0xc2) || (data[1] == 0xc0) || (data[1] == 0xc4)
-    )) {       
-        x_pos = data[3] * 0xFF + data[2];
-        y_pos = data[5] * 0xFF + data[4];
-        pressure = (data[1] != 0xc1) ? pressure_last : data[7] * 0xFF + data[6];
-        
-        pressure_last = pressure;
-        
-        //printk("sensors: x=%05d y=%05d pressure=%05d", x_pos, y_pos, pressure);
-        
-        input_report_abs(idev, ABS_MISC, 0);
-        
-        if ((data[1] == 0xc2) || (data[1] == 0xc4)) {
-            if (stylus_pressed == 0) {
-                if (data[1] == 0xc2) input_report_key(idev_keyboard, BTN_MIDDLE, 1);
-                if (data[1] == 0xc4) input_report_key(idev_keyboard, BTN_RIGHT, 1);
-                input_sync(idev_keyboard);
+    if ((size == 12) && (data[0] == 0x08)) {
+        if (data[1] == 0xe0) {
+            switch (data[4]) {
+                case 0x01:
+                    q11k_rkey_press(KEY_0, data[4]);
+                    break;
+                case 0x02:
+                    q11k_rkey_press(KEY_1, data[4]);
+                    break;
+                case 0x04:
+                    q11k_rkey_press(KEY_2, data[4]);
+                    break;
+                case 0x08:
+                    q11k_rkey_press(KEY_3, data[4]);
+                    break;
+                case 0x10:
+                    q11k_rkey_press(KEY_4, data[4]);
+                    break;
+                case 0x20:
+                    q11k_rkey_press(KEY_5, data[4]);
+                    break;
+                case 0x40:
+                    q11k_rkey_press(KEY_6, data[4]);
+                    break;
+                case 0x80:
+                    q11k_rkey_press(KEY_7, data[4]);
+                    break;
+                default:
+                    printk("Unknown button code captured. Ignored.");
             }
-            stylus_pressed = 1;
-        } else {
-            if (stylus_pressed == 1) {
-                input_report_key(idev_keyboard, BTN_MIDDLE, 0);
-                input_report_key(idev_keyboard, BTN_RIGHT, 0);
-                input_sync(idev_keyboard);
-            }
-            stylus_pressed = 0;
+            return 0;
         }
         
+        x_pos           = data[3] * 0xFF + data[2];
+        y_pos           = data[5] * 0xFF + data[4];
+        pressure        = data[7] * 0xFF + data[6];
         
-        
-        if (data[1] == 0xc0) {
-            input_report_key(idev, BTN_TOOL_PEN, 0);
-            input_report_abs(idev, ABS_PRESSURE, 0);
-        } else {
-            input_report_key(idev, BTN_TOOL_PEN, 1);
-            input_report_abs(idev, ABS_PRESSURE, pressure);
+        DPRINT_DEEP("sensors: x=%08d y=%08d pressure=%08d", x_pos, y_pos, pressure);
+
+        switch (data[1]) {
+            case 0x80:
+                __upress_pen();
+                input_report_key(idev, BTN_TOOL_PEN, 0);
+                input_report_abs(idev, ABS_PRESSURE, 0);
+                break;
+            case 0x81:
+                __upress_pen();
+                input_report_key(idev, BTN_TOOL_PEN, 1);
+                input_report_abs(idev, ABS_PRESSURE, pressure);
+                break;
+            case 0x82:
+                if (stylus_pressed == 0) {
+                    input_report_key(idev_keyboard, BTN_MIDDLE, 1);
+                    input_sync(idev_keyboard);
+                }
+                stylus_pressed = 1;
+                break;
+            case 0x84:
+                if (stylus_pressed == 0) {
+                    input_report_key(idev_keyboard, BTN_RIGHT, 1);
+                    input_sync(idev_keyboard);
+                }
+                stylus_pressed = 1;
+                break;
         }
         
         input_report_abs(idev, ABS_X, x_pos);
         input_report_abs(idev, ABS_Y, y_pos);
-        
         input_sync(idev);
-    }
-
-    if ((size == 8) && (data[0] == 0x0a) && (data[1] == 0xe0) && (data[2] == 0x01) && (data[3] == 0x01)) {
-        key_raw = data[4];
-        switch (key_raw) {
-            case 0x00:
-                // nothing to do...
-                break;
-            case 0x01:
-                q11k_rkey_press(KEY_0, key_raw);
-                break;
-            case 0x02:
-                q11k_rkey_press(KEY_1, key_raw);
-                break;
-            case 0x04:
-                q11k_rkey_press(KEY_2, key_raw);
-                break;
-            case 0x08:
-                q11k_rkey_press(KEY_3, key_raw);
-                break;
-            case 0x10:
-                q11k_rkey_press(KEY_4, key_raw);
-                break;
-            case 0x20:
-                q11k_rkey_press(KEY_5, key_raw);
-                break;
-            case 0x40:
-                q11k_rkey_press(KEY_6, key_raw);
-                break;
-            case 0x80:
-                q11k_rkey_press(KEY_7, key_raw);
-                break;
-            default:
-                printk("Unknown button code captured. Ignored.");
-        }
     }
     
     return 0;
@@ -344,7 +275,7 @@ static void __close_keyboard(void) {
         input_unregister_device(idev_keyboard);
         input_free_device(idev_keyboard);
         idev_keyboard = NULL;
-        printk("Q11K keyboard unregistered");
+        DPRINT("Q11K keyboard unregistered");
     }
 }
 
@@ -353,7 +284,7 @@ static void __close_pad(void) {
         input_unregister_device(idev);
         input_free_device(idev);
         idev = NULL;
-        printk("Q11K tab unregistered");
+        DPRINT("Q11K tab unregistered");
     }
 }
 
@@ -367,18 +298,6 @@ void q11k_remove(struct hid_device *dev) {
     hid_hw_close(dev);
     hid_hw_stop(dev);
 }
-
-/*static int q11k_module_init(void) {
-    return 0;
-}
-
-static void q11k_module_exit(void) {
-    __close_keyboard();
-    __close_pad();
-}*/
-
-//module_init(q11k_module_init);
-//module_exit(q11k_module_exit);
 
 struct wacom_features {
 	const char *name;
@@ -413,10 +332,8 @@ struct wacom_features {
 	int hid_type;
 };
 
-
-//32640
 static const struct wacom_features wacom_features =
-	{ "Wacom Penpartner", 32640, 32640, 8192, 0, 4, 40, 40 };
+	{ "Wacom Penpartner", 55662, 34789, 8192, 0, 4, 40, 40 };
 
 static const struct hid_device_id q11k_device[] = {
     { HID_USB_DEVICE(USB_VENDOR_ID_HUION, USB_DEVICE_ID_HUION_TABLET), .driver_data = (kernel_ulong_t)&wacom_features },
@@ -428,7 +345,6 @@ static struct hid_driver q11k_driver = {
 	.id_table              = q11k_device,
 	.probe                 = q11k_probe,
     .remove                = q11k_remove, 
-	.report_fixup          = q11k_report_fixup,
 	.raw_event             = q11k_raw_event,
 #ifdef CONFIG_PM
 	.resume	               = uclogic_resume,
